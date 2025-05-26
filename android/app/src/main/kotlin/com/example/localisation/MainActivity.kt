9b827b92c497540c10f1a147eedf519bed10bdc2
gabrielloic <loicnlep@gmail.com>
2025-05-26 10:09:06 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/localisation/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.localisation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:location/location.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      home: GeolocationAPP()
+    );
+  }
+}
+
+class GeolocationAPP extends StatefulWidget {
+  const GeolocationAPP({super.key});
+
+  @override
+  State<GeolocationAPP> createState() => _GeolocationAPPState();
+}
+
+class _GeolocationAPPState extends State<GeolocationAPP> {
+  @override
+  Widget build(BuildContext context) {
+    Position? localisation_actuelle;
+    late bool servicePermission;
+    late LocationPermission permission;
+    String adresse_actuelle="";
+
+    Future <Position> Affichage_localisation_actuelle() async {
+      servicePermission= await Geolocator.isLocationServiceEnabled();
+      if(!servicePermission){
+        print("votre service de localisation est désactivé");
+      }
+      permission= await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied){
+        permission = await Geolocator.requestPermission();
+      }
+
+      return await Geolocator.getCurrentPosition();
+    }
+
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Latitude = ${localisation_actuelle?.latitude} ;Longitude= ${localisation_actuelle?.longitude} "),
+            SizedBox(height: 7,),
+            Text(""),
+            SizedBox(height: 7,),
+            ElevatedButton(onPressed:() async{
+              localisation_actuelle= await Affichage_localisation_actuelle();
+              setState(() {
+              });
+              print("${localisation_actuelle!.longitude}");
+            }
+                ,child: Text("affichage localisation")),
+
+          ],
+        )
+        ),
+    );
+  }
+}
